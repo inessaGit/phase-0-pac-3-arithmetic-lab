d63aa78949c058b71ecf8ddf25f8d4e08cf5fbe3
inessa <inessa2209@yahoo.com>
2021-12-02 00:01:18 -0500

    complete assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(){
+
+}
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide(){
+
+}
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b; 
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+function increment (n){
+    //return ++n; 
+    return n+=1;
+}
+function decrement (n){
+    //return --n; 
+    return n-=1; 
+}
+
+function makeInt(n){
+    let integer=parseInt(n,10);
+    return integer;
+}
+
+function   preserveDecimal(n){
+    let float = parseFloat(n);
+    return float;
+}
